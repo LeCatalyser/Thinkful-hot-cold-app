78101a7d1dba3abec59fc3239d922d2a7eb72fc1
Luisa Salcedo <profe.salcedo@gmail.com>
2017-09-19 17:14:41 -0700

    creating my first react app

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      correctNumber: ''
+    }
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <form>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default App;
