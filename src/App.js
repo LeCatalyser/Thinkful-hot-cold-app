79172b629cd6989cab63d4b3f9e8fc8d88d69b7b
Luisa Salcedo <profe.salcedo@gmail.com>
2017-09-19 18:07:04 -0700

    created guess form

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import GuessForm from './guess-form';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      correctNumber: ''
+      correctNumber: 18,
+      Guesses: [],
+      correctGuess: []
     }
   }
-  
+
+
+
   render() {
     return (
       <div className="App">
-        <form>
-        </form>
+        <GuessForm/>
       </div>
     );
   }
---
+++ src/guess-form.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function GuessForm(props) {
+  return (
+    <form>
+      <h1>Hot or Cold</h1>
+      <h2>Make a Guess!</h2>
+      <input placeholder="Enter your Guess" />
+      <label />
+      <button>Guess</button>
+    </form>
+  );
+}
