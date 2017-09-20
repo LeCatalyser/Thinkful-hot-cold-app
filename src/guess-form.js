31effc6f4085e01f0ae0984cfad7c2e6a8d6941f
Luisa Salcedo <profe.salcedo@gmail.com>
2017-09-20 15:28:18 -0700

    added styling to guess-form.js

--- src/guess-form.js
+++ src/guess-form.js
@@ -3,8 +3,8 @@ import React from "react";
 export default function GuessForm(props) {
   return (
     <form>
-      <h1>Hot or Cold</h1>
-      <h2>Make a Guess!</h2>
+      <h1 class="hot">Hot or Cold</h1>
+      <h2 class="guess">Make a Guess!</h2>
       <input placeholder="Enter your Guess" />
       <label />
       <button>Guess</button>
